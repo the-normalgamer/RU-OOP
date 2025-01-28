67e243dc28430d9ddb2ba3ca444e11ffcd7b28b2
the-normalgamer <141769592+the-normalgamer@users.noreply.github.com>
2025-01-28 15:28:20 +0100

    Tests done, life is good

--- app/test/student/Assignment01Tester.java
+++ app/test/student/Assignment01Tester.java
@@ -1,17 +1,20 @@
 package student;
 
 public class Assignment01Tester {
+	private Group group;
 	public void createGroup(int i) {
+		this.group = new Group(i);
 	}
-
 	public void addStudent(int sNumber, String firstName, String lastName) {
+		this.group.addToGroup(new Student (firstName , lastName, sNumber ));
 	}
 
 	public void changeStudent(int sNumber, String firstName, String lastName) {
+		this.group.changeStudent(new Student(firstName,lastName,sNumber));
 	}
 
 	public String printStudents() {
-		return "";
+		return this.group;
 	}
 
 }
