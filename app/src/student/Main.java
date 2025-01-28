2f4bce83665b71fb71299c77ae286fe5c4808643
Phantom <armand.devries@outlook.com>
2025-01-28 14:40:10 +0100

    Student creation

--- app/src/student/Main.java
+++ app/src/student/Main.java
@@ -13,5 +13,21 @@ public class Main {
         int size = scan.nextInt();
 
         Group group = new Group(size);
+
+        for (int i = 0; i < size; i++) {
+            // Prompt the user for a name
+            System.out.println("Enter the name of student " + i);
+            String[] name = scan.toString().split(" ", 2);
+            String fName = name[0];
+            String lName = name[1];
+
+            // Prompt the user for a student number
+            System.out.println("Enter the student number of student " + i);
+            int number = scan.nextInt();
+
+            // Create a new student and add it
+            Student student = new Student(fName, lName, number);
+            group.addStudent(student, i);
+        }
     }
 }
