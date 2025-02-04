849de5964213f80c25bad2e6c4cfc4de20a2b0d2
Phantom <armand.devries@outlook.com>
2025-02-04 11:50:27 +0100

    Assignment 2 files

---
+++ app/src/geometric/Main.java
@@ -0,0 +1,10 @@
+package geometric;
+
+public class Main {
+	// For this assignment, you have to create all the required files yourself.
+	// Tests are available in the test directory.
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
---
+++ app/test/geometric/Assignment02Tester.java
@@ -0,0 +1,58 @@
+package geometric;
+
+public class Assignment02Tester {
+
+	public Assignment02Tester() {
+	}
+
+	public void createCircle(double x, double y, double r) {
+	}
+
+	public void createRectangle(double x, double y, double width, double height) {
+	}
+
+	public double topBorder(int index) {
+		return 0.0;
+	}
+
+	public double rightBorder(int index) {
+		return 0.0;
+	}
+
+	public double bottomBorder(int index) {
+		return 0.0;
+	}
+
+	public double leftBorder(int index) {
+		return 0.0;
+	}
+
+	public double area(int index) {
+		return 0.0;
+	}
+
+	public void move(int index, double dx, double dy) {
+	}
+
+	public void sortByArea() {
+	}
+
+	public void sortByX() {
+	}
+
+	public void sortByY() {
+	}
+
+	public void filterByArea(double threshold) {
+	}
+
+	public void filterByX(double threshold) {
+	}
+
+	public void filterByY(double threshold) {
+	}
+
+	public int getShapeCount() {
+		return 0;
+	}
+}
