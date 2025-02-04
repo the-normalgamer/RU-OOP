c73f0d5ee561ef6db69d194c8a69e8678b200bdb
Phantom <armand.devries@outlook.com>
2025-02-04 13:58:04 +0100

    Start Main loop

--- app/src/geometric/Main.java
+++ app/src/geometric/Main.java
@@ -1,10 +1,60 @@
 package geometric;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 	// For this assignment, you have to create all the required files yourself.
 	// Tests are available in the test directory.
 
+
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		manipulate();
+	}
+
+	public static void manipulate() {
+		Scanner scan = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("> ");
+			String[] input = scan.nextLine().split(" ");
+
+			if (input[0].isEmpty())      continue;
+			if (input[0].equals("quit")) break;
+
+			switch (input[0]) {
+				case "show": {
+					System.out.println("Entered show command");
+					break;
+				}
+				case "circle": {
+					System.out.println("Entered circle command");
+					break;
+				}
+				case "rectangle": {
+					System.out.println("Entered rectangle command");
+					break;
+				}
+				case "move": {
+					System.out.println("Entered move command");
+					break;
+				}
+				case "remove": {
+					System.out.println("Entered remove command");
+					break;
+				}
+				case "filter": {
+					System.out.println("Entered filter command");
+					break;
+				}
+				case "sort": {
+					System.out.println("Entered sort command");
+					break;
+				}
+				default: System.out.println("Unknown command: '" + input[0] + "'! Please try again.");
+			}
+
+//			System.out.println("Got: " + Arrays.toString(input));
+		}
 	}
 }
