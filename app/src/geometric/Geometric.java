d040b18fcd753bc0ab92e71db6a2a1ceb28c202b
Phantom <armand.devries@outlook.com>
2025-02-04 12:51:27 +0100

    Base classes

---
+++ app/src/geometric/Geometric.java
@@ -0,0 +1,28 @@
+package geometric;
+
+public interface Geometric {
+    /**
+     * @return The area of the geometric shape.
+     */
+    double getArea();
+
+    enum Side {
+        TOP,
+        BOTTOM,
+        LEFT,
+        RIGHT,
+    }
+
+    /**
+     * @param side Which side to return.
+     * @return The x or y value of the border of the smallest rectangle surrounding the geometric shape.
+     */
+    double getBorder(Side side);
+
+    /**
+     * Moves the geometric shape on the x- and y-axis.
+     * @param dx How much to move horizontally.
+     * @param dy How much to move vertically.
+     */
+    void moveObject(double dx, double dy);
+}
---
+++ app/src/geometric/GeometricPredicate.java
@@ -0,0 +1,5 @@
+package geometric;
+
+public interface GeometricPredicate {
+    boolean predicate(Geometric shape);
+}
