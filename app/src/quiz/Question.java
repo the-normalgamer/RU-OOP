68fddc3f961e5c205a69509b375f897bafad9ddd
the-normalgamer <141769592+the-normalgamer@users.noreply.github.com>
2025-02-18 14:38:38 +0100

    Question methods

--- app/src/quiz/Question.java
+++ app/src/quiz/Question.java
@@ -1,4 +1,25 @@
 package quiz;
 
 public class Question {
+    private int score = 0;
+
+    public String toString(){
+        return "";
+        //TODO
+    }
+    public boolean isCorrect(String answer){
+        return true;
+        //TODO
+    }
+    public String correctAnswer(){
+        return "";
+        //TODO
+    }
+    public int getScore(){
+        return this.score;
+    }
+    public void setScore(int val){
+        score = val;
+    }
+
 }
