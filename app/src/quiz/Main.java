ca533c33ec6a5b7d1a58cceb1e4cbab2f1eaae14
the-normalgamer <141769592+the-normalgamer@users.noreply.github.com>
2025-02-18 14:26:35 +0100

    Assignment 4 start

---
+++ app/src/quiz/Main.java
@@ -0,0 +1,11 @@
+package quiz;
+
+public class Main {
+	// You need to create the question classes yourself.
+	// You can use the tests in the `test` directory to check your implementation.
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
