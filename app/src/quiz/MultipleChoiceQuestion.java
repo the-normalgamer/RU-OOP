edd036548f6cf443a0eef4d9e684994aa2326ea2
Phantom <armand.devries@outlook.com>
2025-02-18 16:22:39 +0100

    Finalize question definitions

--- app/src/quiz/MultipleChoiceQuestion.java
+++ app/src/quiz/MultipleChoiceQuestion.java
@@ -3,14 +3,32 @@ package quiz;
 public class MultipleChoiceQuestion extends Question {
     protected String[] answers;
 
-    public MultipleChoiceQuestion (String question, String [] answers, int correctAnswer, int score){
+    /**
+     * Multiple choice question where one question is correct
+     * @param question The question to prompt the user with.
+     * @param answers List of possible answers.
+     * @param correctAnswer Index of {@code answers} which points to the correct answer.
+     *                      If negative, every answer is considered correct.
+     * @param score The number of points to award when the correct answer is chosen.
+     */
+    public MultipleChoiceQuestion (String question, String[] answers, int correctAnswer, int score){
         this.question = question;
         this.answers = answers;
-        this.correctAnswer = answers[correctAnswer];
-        this.evaluation = str -> str.equalsIgnoreCase(this.correctAnswer) ? score : 0;
+        this.correctAnswer = (correctAnswer >= 0) ? answers[correctAnswer] : "";
+        this.evaluation = str -> {
+            if (this.correctAnswer.isEmpty()) return score;
+            return str.equalsIgnoreCase(this.correctAnswer) ? score : 0;
+        };
     }
 
-    public MultipleChoiceQuestion (String question, String [] answers, int exampleAnswer){
-        new MultipleChoiceQuestion(question, answers, exampleAnswer, DEFAULT_POINTS);
+    /**
+     * Multiple choice question where one question is correct<br><br>
+     * Will set default score of {@value DEFAULT_POINTS}
+     * @param question The question to prompt the user with
+     * @param answers List of possible answers
+     * @param correctAnswer Index of {@code answers} which points to the correct answer
+     */
+    public MultipleChoiceQuestion (String question, String[] answers, int correctAnswer){
+        new MultipleChoiceQuestion(question, answers, correctAnswer, DEFAULT_POINTS);
     }
 }
--- app/src/quiz/ThisThatQuestion.java
+++ app/src/quiz/ThisThatQuestion.java
@@ -1,10 +1,29 @@
 package quiz;
 
 public class ThisThatQuestion extends MultipleChoiceQuestion{
-    ThisThatQuestion (String question, String answer1, String answer2 , int correctAnswer , int score){
-
+    /**
+     * Multiple choice question with only two options.
+     * @param question The question to prompt the user with.
+     * @param answerA Second possible answer.
+     * @param answerB Second possible answer.
+     * @param correctAnswer Which of the two answers is correct, 0 or 1.
+     *                      If negative, every answer is considered correct.
+     * @param score The number of points to award when the correct answer is chosen.
+     */
+    ThisThatQuestion(String question, String answerA, String answerB, int correctAnswer, int score) {
+        super(question, new String[]{answerA, answerB}, correctAnswer, score);
     }
-    ThisThatQuestion ( String question, String answer1, String answer2 , int correctAnswer ){
-        ThisThatQuestion(question, answer1, answer2, correctAnswer, 5)
+
+    /**
+     * Multiple choice question with only two options.<br><br>
+     * Will set default score of {@value DEFAULT_POINTS}
+     * @param question The question to prompt the user with.
+     * @param answerA Second possible answer.
+     * @param answerB Second possible answer.
+     * @param correctAnswer Which of the two answers is correct, 0 or 1.
+     *                      If negative, every answer is considered correct.
+     */
+    ThisThatQuestion(String question, String answerA, String answerB, int correctAnswer) {
+        this(question, answerA, answerB, correctAnswer, DEFAULT_POINTS);
     }
 }
