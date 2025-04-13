fa3d2f030341c40ca955d415e60947aec85d590c
the-normalgamer <141769592+the-normalgamer@users.noreply.github.com>
2025-04-13 13:20:29 +0200

    tree nodes start

---
+++ app/src/quadtrees/BlackLeaf.java
@@ -0,0 +1,24 @@
+package quadtrees;
+
+import java.io.Writer;
+
+public class BlackLeaf implements QuadTreeNode {
+    /**
+     * @param x
+     * @param y
+     * @param width
+     * @param bitmap
+     */
+    @Override
+    public void fillBitmap(int x, int y, int width, Bitmap bitmap) {
+
+    }
+
+    /**
+     * @param out
+     */
+    @Override
+    public void writeNode(Writer out) {
+
+    }
+}
