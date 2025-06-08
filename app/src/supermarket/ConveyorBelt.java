dde32a9c1c527e0741d29aca063ce1ea9e41451f
Phantom <armand.devries@outlook.com>
2025-06-08 12:21:43 +0200

    locking conditioning

--- app/src/supermarket/ConveyorBelt.java
+++ app/src/supermarket/ConveyorBelt.java
@@ -9,7 +9,8 @@ public class ConveyorBelt<T> {
 	private final Object[] elements;
 	private int amount, begin, end;
 	private final Lock lock = new ReentrantLock();
-	private final Condition empty = lock.newCondition();
+	private final Condition notEmpty = lock.newCondition();
+	private final Condition notFull = lock.newCondition();
 
 	public ConveyorBelt(int size) {
 		elements = new Object[size];
@@ -19,15 +20,40 @@ public class ConveyorBelt<T> {
 	}
 
 	public void putIn(T item) {
-		elements[end] = item;
-		end = (end + 1) % elements.length;
-		amount = amount + 1;
+		lock.lock();
+		try {
+			while (amount == elements.length) notFull.await();
+
+			elements[end] = item;
+			end = (end + 1) % elements.length;
+			amount += 1;
+
+			notEmpty.signal();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	public T removeFrom() { // Assumes there is at least one element
-		T item = (T) elements[begin];
-		begin = (begin + 1) % elements.length;
-		amount = amount - 1;
-		return item;
+		lock.lock();
+		try {
+			while (amount == 0) notEmpty.await();
+
+			T item = (T) elements[begin];
+			begin = (begin + 1) % elements.length;
+			amount -= 1;
+
+			notFull.signal();
+
+			return item;
+		} catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+			lock.unlock();
+		}
+
+		return null;
 	}
 }
