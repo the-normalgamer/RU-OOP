312a9e944e6db68c665dad94f6ce519dfbd7438e
Phantom <armand.devries@outlook.com>
2025-06-08 12:09:32 +0200

    Generic generics

--- app/src/supermarket/ConveyorBelt.java
+++ app/src/supermarket/ConveyorBelt.java
@@ -1,6 +1,6 @@
 package supermarket;
 
-public class ConveyorBelt {
+public class ConveyorBelt<T> {
 
 	private final Object[] elements;
 	private int amount, begin, end;
@@ -12,14 +12,14 @@ public class ConveyorBelt {
 		end = 0;
 	}
 
-	public void putIn(Object item) {
+	public void putIn(T item) {
 		elements[end] = item;
 		end = (end + 1) % elements.length;
 		amount = amount + 1;
 	}
 
-	public Object removeFrom() { // Assumes there is at least one element
-		Object item = elements[begin];
+	public T removeFrom() { // Assumes there is at least one element
+		T item = (T) elements[begin];
 		begin = (begin + 1) % elements.length;
 		amount = amount - 1;
 		return item;
--- app/src/supermarket/Register.java
+++ app/src/supermarket/Register.java
@@ -8,8 +8,8 @@ public class Register {
 	// Make sure that CONVEYOR_SIZE + BIN_SIZE >= Customer.MAX_ITEMS, otherwise
 	// danger of deadlock
 	private static final int CONVEYER_SIZE = 10, BIN_SIZE = 10;
-	private ConveyorBelt belt = new ConveyorBelt(CONVEYER_SIZE);
-	private ConveyorBelt bin = new ConveyorBelt(CONVEYER_SIZE);
+	private ConveyorBelt<Item> belt = new ConveyorBelt<>(CONVEYER_SIZE);
+	private ConveyorBelt<Item> bin = new ConveyorBelt<>(CONVEYER_SIZE);
 	private Lock claimed = new ReentrantLock();
 
 	public void putOnBelt(Item article) {
